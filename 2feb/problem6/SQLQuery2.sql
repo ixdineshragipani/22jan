2ccfb1a4e29f8181701cf20050f69b8be4696cf9
ixdineshragipani <dinesh.ragipani@innovatechs.com>
2026-02-03 10:15:49 +0530

    problem6 saved

---
+++ 2feb/problem6/SQLQuery2.sql
@@ -0,0 +1,9 @@
+select * from students where age in (select age from students where age>20 );
+
+select count(*) from students where name in (select name from students where name like '%a%');
+
+select * from courses where fees in (select fees from courses where fees between 120000 and 140000) order by fees;
+
+select * from students where age is not null (select * from students where active_flag=0);
+
+select * from enrolements where course_id in (select course_id from enrolements group by course_id having count(*)>1)vs
